f612b2dc825ab46010e66226243a66b10d0f71dd
ericlent <ericquaresma@hotmail.com>
2024-08-21 09:27:40 -0300

    Alterações para rodar em outro ambiente

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=d:\TFS\AiAgent\.venv
+set VIRTUAL_ENV=f:\ProjetosAI\AiAgent\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
